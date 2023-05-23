5b97f3e8bbde5fa5e5454a1bdcd64ec74559cd73
SK <134377909+SuriyaKL27@users.noreply.github.com>
2023-05-23 16:41:31 +0200

    Add files via upload

---
+++ Ex 1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+
+    srand(time(0));
+    int secretAmount = rand() % 100 + 1;
+
+    printf("Bienvenue dans le jeu Trouver le Juste Prix !\n");
+    printf("Un montant entre 1 et 100 euros a été choisi. A vous de le deviner.\n");
+
+    int playerGuess;
+    int attempts = 0;
+    bool hasWon = false;
+
+    while (!hasWon) {
+        printf("Entrez votre estimation : ");
+        scanf("%d", &playerGuess);
+        
+        attempts++;
+
+        if (playerGuess == secretAmount) {
+            printf("Félicitations ! Vous avez trouvé le Juste Prix .\n", attempts);
+            hasWon = true;
+        } else if (playerGuess < secretAmount) {
+            printf("C'est + .\n");
+        } else {
+            printf("C'est - .\n");
+        }
+    }
+
+    return 0;
+}
