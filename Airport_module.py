789bea1b7098df9e65878a18b6ca187e77dd8e78
SK <134377909+SuriyaKL27@users.noreply.github.com>
2023-05-24 09:38:43 +0200

    Delete Airport_module.py

--- Airport_module.py
+++
@@ -1,9 +0,0 @@
-class Airport:
-    def __init__(self):
-        self.name = "Dubai International Airport"
-        self.location = "Dubai"
-        self.runways = 3
-        self.terminals = 3
-        self.gates = 7
-        self.airplanes = 1
-        self.destinations = ["New York", "London", "Chennai", "Paris", "Dubai"]
